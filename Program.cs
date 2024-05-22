26cb570e8fb6687c64e85e23ca8ca6eca3305784
Marcos Bergami <56553101+marcosbergami@users.noreply.github.com>
2024-05-22 15:22:18 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new { Name = "John Doe", Age = 30 };
+
+            string json = JsonConvert.SerializeObject(person, Formatting.Indented);
+            
+            Console.WriteLine(json);
+        }
+    }
+}
